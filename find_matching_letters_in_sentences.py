235bddf7189944d5a0c3d6e6249fe975319d6d0a
Victor Barkalov <120062506+vicbark@users.noreply.github.com>
2023-05-10 22:05:45 +0300

    Update find_matching_letters_in_sentences.py

--- find_matching_letters_in_sentences.py
+++ find_matching_letters_in_sentences.py
@@ -4,6 +4,6 @@ b=input("Enter the second sentence ")
 c="" # here we write the matching letters in sentences / kuda budem zapisivatj sovpodajuwie bukvi
 for x in a: # iterate over the line "a" / pereberaem stroku "a"
     if x in b and x not in c: # if the character is in the string "b" but has not yet been added to "c" /
-        # esli simvol estj v stroke "b" i ewe ne dobavlen v c
+        # esli simvol estj v stroke "b" i ewe ne dobavlen v "c"
         c += x # add character to new line / pri sovpodenie dobavljaem simvol v novuju stroku
 print(c) # matching characters / pishem sovpadajuwie simvoli       
